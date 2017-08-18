87f678a7a47eb6841cae0eec66fc8c1d666252a4
Vince-V <vincevillas2@gmail.com>
2017-08-17 23:43:21 -0500

    addedServiceClass

---
+++ src/com/userInfo/CoolUserService.java
@@ -0,0 +1,13 @@
+package com.userInfo;
+
+public class CoolUserService {
+	
+	public CoolUser getCoolUser()
+	{
+		CoolUser coolUser = new CoolUser();
+		coolUser.setFirstName("");
+		coolUser.setLastName("");
+		return coolUser;
+	}
+
+}
